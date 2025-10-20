cbd45e6fb6845766c02dcccc4ca52cbbad50fae2
ramiemosely5@gmail.com <ramiemosely5@gmail.com>
2025-10-19 20:08:01 -0400

    Added graphics

--- mygame.py
+++ mygame.py
@@ -1,16 +1,27 @@
 import pygame
 from sys import exit
 
+#Put Objects in variables
 pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('ULTRA')
 clock = pygame.time.Clock()
 
+#Create a surface and import image
+testSurface = pygame.image.load('graphics/Sky.png')
+
+
+
+#Main Gameplay Loop
 while True:
+    #Check if user quits
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
+    screen.blit(testSurface,(200,100))
+
     #draw all our elements update everything
     pygame.display.update()
     clock.tick(60)
